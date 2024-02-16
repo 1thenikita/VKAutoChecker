8b1374d6fcf32f3f15f0268311e05cfa7eadc15c
Nikita Shidlovsky <nik-801@yandex.ru>
2024-02-16 14:01:56 +0300

    Fix logging.

--- VKAutoChecker/Program.cs
+++ VKAutoChecker/Program.cs
@@ -2,7 +2,12 @@ using Serilog;
 using VKAutoChecker;
 using VKAutoChecker.Services;
 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
 var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddSerilog();
 builder.Services.AddSerilog();
 // builder.Services.AddHostedService<Worker>();
 
